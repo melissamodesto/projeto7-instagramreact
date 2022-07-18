f7f6e27f082e7bb6666e9ee5182c3c3d545e7fe4
Melissa Modesto <mbm.modesto@gmail.com>
2022-07-18 09:56:39 -0300

    MobileBackground

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Container from './Container'; // Importando o componente Container
 import Header from './Header'; // Importando o componente Header 
+import MobileBackground from './MobileBackground'; 
 
 
 function App() {
